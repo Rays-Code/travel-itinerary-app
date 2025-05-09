14779e1d893efe7bc1b2b735ae9c75f99a516e5e
Dipraj Ray <raydipraj1234@gmail.com>
2025-05-09 22:34:49 +0530

    fixed alignment issue in StatusBar

--- src/components/common/StatusBar.jsx
+++ src/components/common/StatusBar.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const StatusBar = () => {
   return (
-    <div className='w-[377px] h-[24px] pl-2 py-2 flex justify-between'>
+    <div className='w-[370px] h-[24px] pl-2 py-2 flex justify-between'>
 
         {/* icon-left */}
         <div className='w-[35px] h-[22px] top-[1px] left-[8px]'>
-        <span className='text-[#0D0D0E] dark:text-[#FFFFFF] text-[17px] font-sf font-semibold pl-4'>9:41</span>
+        <span className='text-[#0D0D0E] dark:text-[#FFFFFF] text-[17px] font-sf font-semibold pl-1'>9:41</span>
         </div>
 
         {/* icon-left */}
